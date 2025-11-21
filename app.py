d79fc8e3a350779a23aaf1d894c40e56e390170e
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2025-11-21 21:35:23 +0530

    add monitoring endpoints

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import request
+import logging
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, DevOps!"
+
+@app.route('/health')
+def health():
+    return{"status": "up"}, 200
+
+logging.basicConfig(filename="app.log", level=logging.INFO)
+
+@app.before_request
+def log_request():
+    logging.info(f"Request: {request.method} {request.path}")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
